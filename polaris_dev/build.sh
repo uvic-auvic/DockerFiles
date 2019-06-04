b3cf574843c3c57ce0d65700076f3fd5848d0c72
Alec <avlecxk@gmail.com>
2019-06-04 13:25:23 -0700

    Updated docker to use /var instead of /usr, added bin/bash to the scripts.

--- polaris_dev/build.sh
+++ polaris_dev/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker login
 docker build --tag polaris_dev .
 docker tag polaris_dev auvic/polaris:polaris_dev
--- polaris_run/build.sh
+++ polaris_run/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 docker login
 docker build --tag polaris_run .
 docker tag polaris_run auvic/polaris:polaris_run
